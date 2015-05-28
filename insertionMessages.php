e821d7c6f2547195325290f7d30083a3395ee044
RemiPeral <remi.peral@gmail.com>
2015-05-28 16:03:16 +0200

    insertionMessage

--- insertionMessages.php
+++ insertionMessages.php
@@ -3,12 +3,13 @@ ob_start();
 	require("connexionBDD.php");
 	
 	// On récupère les données saisies dans le formulaire
-	$titre = $_POST['titre'];
-	$lien = $_POST['lien'];
-	$description = $_POST['description'];
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
 	
 	// On récupère dans la base de données le mot de passe qui correspond au nom saisi par le visiteur
-	$sql = "INSERT INTO `germn`.`message` (`id`, `titre`, `lien`, `description`) VALUES (NULL, '$titre', '$lien', '$description');";
+	$sql = "INSERT INTO `germn`.`message` (`id`, `nom`, `email`, `sujet`, `message`) VALUES (NULL, '$name', '$email', '$subject', '$message');";
 	$result = $connexion->exec($sql) or die ('Erreur dans la requête SQL' . $sql);
 
 	header("location:interface.php");
